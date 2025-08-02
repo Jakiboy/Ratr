c189e25ecfd670268523b46798ca4b75a8fb21fa
Jakiboy <j.sinnaour.official@gmail.com>
2025-08-02 21:28:05 +0100

    Update build.sh

--- src/Decoder/build.sh
+++ src/Decoder/build.sh
@@ -14,7 +14,7 @@ git clone https://github.com/Jakiboy/Ztedecode.git zte
 pip install ./zte
 python -m PyInstaller --onefile --noconsole --icon=router.ico ./zte/decode.py
 mv ./dist/decode.exe ./zte.exe
-rm -f decode.spec
+rm -f *.spec
 rm -rf build dist zte
 sleep 2
 clear
@@ -25,7 +25,10 @@ git clone https://github.com/Jakiboy/Hwdecode.git huawei
 pip install ./huawei
 python -m PyInstaller --onefile --noconsole --icon=router.ico --hidden-import=Crypto ./huawei/Hwdecode.py
 mv ./dist/Hwdecode.exe ./huawei.exe
-rm -f decode.spec
+rm -f *.spec
 rm -rf build dist huawei
 sleep 2
 clear
+
+echo -e "${G}All executables have been generated successfully!"
+read -p "Press any key to exit..."
